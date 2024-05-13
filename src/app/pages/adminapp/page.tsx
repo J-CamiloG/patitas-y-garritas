ef6f5879014a12200a3a729e13ed7c94ff6b9ad9
J-CamiloG <jc6381878@gmail.com>
2024-05-13 09:01:28 -0500

    Seccion de lformilario de ingreso, se le agregaron los stilos pendinete la logica

--- src/app/pages/adminapp/page.tsx
+++ src/app/pages/adminapp/page.tsx
@@ -10,7 +10,7 @@ export default function AdminApp() {
                 {/* <p className="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet providers or create a new one.</p> */}
                 <ul className="my-4 space-y-3">
                     <li>
-                        <Link href="pages/admit" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                        <Link href="/pages/admit" className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
                             <span className="flex-1 ms-3 whitespace-nowrap">Admit patient</span>
                         </Link>
                     </li>
